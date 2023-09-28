655d14abc4536eff3dfdbcedd916a68e91a6eea9
JleseelleurQUB <josephleseelleur@gmail.com>
2023-09-28 21:07:23 +0100

    Update 3
    - Changed repeating invoke method for ball spawn to a single invoke, and changed the function to be looping, with the invoke delay changing on each loop
    
    - Added additional variables to player for the time that last dog was spawned and a cooldown of 0.5 seconds, added additional layer to if statement checking whether the current time exceeded this value + cooldown

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float dogUsed = 0f;
+    private float dogCooldown = 0.5f;
 
     // Update is called once per frame
     void Update()
@@ -12,7 +14,12 @@ public class PlayerControllerX : MonoBehaviour
         // On spacebar press, send dog
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            if(Time.time > dogUsed + dogCooldown) // If the current time is greater than the time of the last dog plus the cooldown, another dog is spawned
+            {
+                dogUsed = Time.time; // Updates the time of the last dog
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+            
         }
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SpawnManagerX : MonoBehaviour
 {
@@ -9,24 +10,29 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
     private float spawnPosY = 30;
-
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private float spawnInterval;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        // Assigns a random value to spawnInterval variable, carries out spawn function once after this time has passed
+        spawnInterval = Random.Range(3, 5);
+        Invoke("SpawnRandomBall", spawnInterval);
     }
 
-    // Spawn random ball at random x position at top of play area
+    // Function to spawn random ball at random x position at top of play area, looping
     void SpawnRandomBall ()
     {
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
 
-        // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        // Instantiate ball at random spawn location
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+
+        // Set new random spawnInterval, repeats function once after this time has passed, creating loop
+        spawnInterval = Random.Range(3, 5);
+        Invoke("SpawnRandomBall", spawnInterval);
     }
 
 }
